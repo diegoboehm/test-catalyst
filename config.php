997d9c733905f50c1c1115e9a2ec828d9e37495e
Diego Boehm <diegoboehm@MacBook-Air-M1-de-Diego.local>
2022-07-15 14:10:20 +1000

    Connection and configuration files

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * DATABASE
+ */
+define("CONF_DB_HOST", "localhost");
+define("CONF_DB_USER", "root");
+define("CONF_DB_PASS", "");
+define("CONF_DB_NAME", "test-catalyst");
+
+/**
+ * PROJECT URLs
+ */
+define("CONF_URL_BASE", "https://www.localhost/test-catalyst");
+
+/**
+ * SITE
+ */
+define("CONF_SITE_NAME", "Test PHP Catalyst");
+define("CONF_SITE_TITLE", "Test PHP Catalyst");
+define("CONF_SITE_DESC", "Testing my knowledge with php and DB.");
+
